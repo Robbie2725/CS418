a16e526193834f9e9cc12b1620dd97f4aa276392
Robbie2725 <rkrokos2725@gmail.com>
2020-03-11 15:07:44 -0500

    Added p2 folder, fixed fog/terrain, updated matrix library

---
+++ mp2_p2/Terrain.js
@@ -0,0 +1,376 @@
+/**
+ * @fileoverview Terrain - A simple 3D terrain using WebGL
+ * @author Eric Shaffer, Robbie Krokos (rkroko2)
+ */
+
+/** Class implementing 3D terrain. */
+class Terrain{
+/**
+ * Initialize members of a Terrain object
+ * @param {number} div Number of triangles along x axis and y axis
+ * @param {number} minX Minimum X coordinate value
+ * @param {number} maxX Maximum X coordinate value
+ * @param {number} minY Minimum Y coordinate value
+ * @param {number} maxY Maximum Y coordinate value
+ */
+    constructor(div,minX,maxX,minY,maxY){
+        this.div = div;
+        this.minX=minX;
+        this.minY=minY;
+        this.maxX=maxX;
+        this.maxY=maxY;
+
+        // Allocate point to generate random plane
+        this.p = [0, 0, 0];
+	// Allocate a vector for the normal to the random plane
+        this.n = [0, 0, 0];
+        // Allocate vertex array
+        this.vBuffer = [];
+        // Allocate triangle array
+        this.fBuffer = [];
+        // Allocate normal array
+        this.nBuffer = [];
+        // Allocate array for edges so we can draw wireframe
+        this.eBuffer = [];
+        console.log("Terrain: Allocated buffers");
+
+        this.generateTriangles();
+        console.log("Terrain: Generated triangles");
+        this.randomTerrain(); // make the terrain not flat
+        this.setVertexNormals(); // update the vertex normals of the new terrain
+
+        this.generateLines();
+        console.log("Terrain: Generated lines");
+
+        // Get extension for 4 byte integer indices for drwElements
+        var ext = gl.getExtension('OES_element_index_uint');
+        if (ext ==null){
+            alert("OES_element_index_uint is unsupported by your browser and terrain generation cannot proceed.");
+        }
+    }
+
+    /**
+    * Set the x,y,z coords of a vertex at location(i,j)
+    * @param {Object} v an an array of length 3 holding x,y,z coordinates
+    * @param {number} i the ith row of vertices
+    * @param {number} j the jth column of vertices
+    */
+    setVertex(v,i,j)
+    {
+        // Calculate the index in the vBuffer
+        var idx = 3*(i*(this.div+1)+j);
+        // Set the point in the vBuffer to v
+        this.vBuffer[idx]=v[0];
+        this.vBuffer[idx+1]=v[1];
+        this.vBuffer[idx+2]=v[2];
+    }
+
+    /**
+    * Return the x,y,z coordinates of a vertex at location (i,j)
+    * @param {Object} v an an array of length 3 holding x,y,z coordinates
+    * @param {number} i the ith row of vertices
+    * @param {number} j the jth column of vertices
+    */
+    getVertex(v,i,j)
+    {
+        // Calculate the index
+        var idx = 3*(i*(this.div+1)+j);
+        // Set v to the point in the vBuffer
+        v[0]=this.vBuffer[idx];
+        v[1]=this.vBuffer[idx+1];
+        v[2]=this.vBuffer[idx+2];
+    }
+
+    /**
+    * Send the buffer objects to WebGL for rendering
+    */
+    loadBuffers()
+    {
+        // Specify the vertex coordinates
+        this.VertexPositionBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.VertexPositionBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.vBuffer), gl.STATIC_DRAW);
+        this.VertexPositionBuffer.itemSize = 3;
+        this.VertexPositionBuffer.numItems = this.numVertices;
+        console.log("Loaded ", this.VertexPositionBuffer.numItems, " vertices");
+
+        // Specify normals to be able to do lighting calculations
+        this.VertexNormalBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.VertexNormalBuffer);
+        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.nBuffer),
+                  gl.STATIC_DRAW);
+        this.VertexNormalBuffer.itemSize = 3;
+        this.VertexNormalBuffer.numItems = this.numVertices;
+        console.log("Loaded ", this.VertexNormalBuffer.numItems, " normals");
+
+        // Specify faces of the terrain
+        this.IndexTriBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.IndexTriBuffer);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint32Array(this.fBuffer),
+                  gl.STATIC_DRAW);
+        this.IndexTriBuffer.itemSize = 1;
+        this.IndexTriBuffer.numItems = this.fBuffer.length;
+        console.log("Loaded ", this.IndexTriBuffer.numItems, " triangles");
+
+        //Setup Edges
+        this.IndexEdgeBuffer = gl.createBuffer();
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.IndexEdgeBuffer);
+        gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint32Array(this.eBuffer),
+                  gl.STATIC_DRAW);
+        this.IndexEdgeBuffer.itemSize = 1;
+        this.IndexEdgeBuffer.numItems = this.eBuffer.length;
+
+        console.log("triangulatedPlane: loadBuffers");
+    }
+
+    /**
+    * Render the triangles
+    */
+    drawTriangles(){
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.VertexPositionBuffer);
+        gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, this.VertexPositionBuffer.itemSize,
+                         gl.FLOAT, false, 0, 0);
+
+        // Bind normal buffer
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.VertexNormalBuffer);
+        gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute,
+                           this.VertexNormalBuffer.itemSize,
+                           gl.FLOAT, false, 0, 0);
+
+        //Draw
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.IndexTriBuffer);
+        gl.drawElements(gl.TRIANGLES, this.IndexTriBuffer.numItems, gl.UNSIGNED_INT,0);
+    }
+
+    /**
+    * Render the triangle edges wireframe style
+    */
+    drawEdges(){
+
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.VertexPositionBuffer);
+        gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, this.VertexPositionBuffer.itemSize,
+                         gl.FLOAT, false, 0, 0);
+
+        // Bind normal buffer
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.VertexNormalBuffer);
+        gl.vertexAttribPointer(shaderProgram.vertexNormalAttribute,
+                           this.VertexNormalBuffer.itemSize,
+                           gl.FLOAT, false, 0, 0);
+
+        //Draw
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.IndexEdgeBuffer);
+        gl.drawElements(gl.LINES, this.IndexEdgeBuffer.numItems, gl.UNSIGNED_INT,0);
+    }
+/**
+ * Fill the vertex and buffer arrays
+ */
+generateTriangles()
+{
+    //Your code here
+    var dx=(this.maxX-this.minX)/this.div;
+    var dy=(this.maxY-this.minY)/this.div;
+
+    for(var i=0; i<=this.div; i++){
+      for(var j=0; j<=this.div; j++){
+        this.vBuffer.push(this.minX+dx*j);
+        this.vBuffer.push(this.minY+dy*i);
+        this.vBuffer.push(0);
+
+        this.nBuffer.push(0);
+        this.nBuffer.push(0);
+        this.nBuffer.push(0);
+
+      }
+    }
+
+    for(var i=0; i<this.div; i++){
+      for(var j=0; j<this.div; j++){
+        var idx=i*(this.div+1)+j;
+        this.fBuffer.push(idx);
+        this.fBuffer.push(idx+1);
+        this.fBuffer.push(idx+this.div+1);
+
+        this.fBuffer.push(idx+1);
+        this.fBuffer.push(idx+1+this.div+1);
+        this.fBuffer.push(idx+this.div+1);
+
+      }
+    }
+    //
+    this.numVertices = this.vBuffer.length/3;
+    this.numFaces = this.fBuffer.length/3;
+}
+
+/**
+ * Print vertices and triangles to console for debugging
+ */
+printBuffers()
+    {
+
+    for(var i=0;i<this.numVertices;i++)
+          {
+           console.log("v ", this.vBuffer[i*3], " ",
+                             this.vBuffer[i*3 + 1], " ",
+                             this.vBuffer[i*3 + 2], " ");
+
+          }
+
+      for(var i=0;i<this.numFaces;i++)
+          {
+           console.log("f ", this.fBuffer[i*3], " ",
+                             this.fBuffer[i*3 + 1], " ",
+                             this.fBuffer[i*3 + 2], " ");
+
+          }
+
+    }
+
+/**
+ * Generates line values from faces in faceArray
+ * to enable wireframe rendering
+ */
+generateLines()
+{
+    var numTris=this.fBuffer.length/3;
+    for(var f=0;f<numTris;f++)
+    {
+        var fid=f*3;
+        this.eBuffer.push(this.fBuffer[fid]);
+        this.eBuffer.push(this.fBuffer[fid+1]);
+
+        this.eBuffer.push(this.fBuffer[fid+1]);
+        this.eBuffer.push(this.fBuffer[fid+2]);
+
+        this.eBuffer.push(this.fBuffer[fid+2]);
+        this.eBuffer.push(this.fBuffer[fid]);
+    }
+
+}
+
+generatePlane(){
+  // random point from range (minX, minY, 0) to (maxX, maxY, 0)
+  this.p = [Math.random()*(this.maxX-this.minX)+this.minX, Math.random()*(this.maxY-this.minY)+this.minY, 0];
+
+  //random unit vector for the normal vector
+  var normal = [Math.random()-.5, Math.random()-.5, 0]; //get random 2d vector
+  var magnitude = Math.sqrt(normal[0]*normal[0] + normal[1]*normal[1]);
+  this.n = [normal[0]/magnitude, normal[1]/magnitude, 0]; //normalize
+}
+
+randomTerrain(){
+  var delta=.008; //used to increase or decrease z component
+  // Generate 100 random plains
+  for(var i=0; i<200; i++){
+    this.generatePlane(); //generate random plane
+
+    //Loop through each point in the terrain
+    for(var x=0; x<=this.div; x++){
+      for(var y=0; y<=this.div; y++){
+        //get the current vertex at x,y
+        var vtx = [0,0,0];
+        this.getVertex(vtx, x, y);
+
+        //get vector from plain to point
+        var v_p = [vtx[0]-this.p[0], vtx[1]-this.p[1], vtx[2]-this.p[2]] ;
+
+        // Calculate dot product of plain to point vector and normal vector
+        var result=0;
+        for (var j = 0; j < 3; j++) {
+          result += v_p[j] * this.n[j];
+        }
+
+        // If above plain, add delta
+        if(result>0) {
+          vtx[2]+=delta;
+        }
+        else { //else subtract delta
+          vtx[2]-=delta;
+        }
+
+        // Set the new z coordinate
+        this.setVertex(vtx, x, y);
+      }
+    }
+  }
+}
+
+setVertexNormals(){
+
+  // Loop through each 'square' of the terrain grid
+  for(var i=0; i<this.div; i++){
+    for(var j=0; j<this.div; j++){
+
+      // vars used to get each traingles vertices
+      var v1 = [0,0,0];
+      var v2 = [0,0,0];
+      var v3 = [0,0,0];
+
+      // var used to compute the cross product
+      var prod=[0,0,0];
+
+      // get uppper triangles vertices
+      this.getVertex(v1, i, j);
+      this.getVertex(v2, i+1, j+1);
+      this.getVertex(v3, i+1, j);
+
+      //Get the cross product of (v2-v1)x(v3-v1)
+      cross_prod(prod, [v2[0]-v1[0], v2[1]-v1[1], v2[2]-v1[2]], [v3[0]-v1[0], v3[1]-v1[1], v3[2]-v1[2]]);
+
+      // get index of each vertex in nBuffer
+      var idx1 = 3*(i*(this.div+1)+j);
+      var idx2 = 3*((i+1)*(this.div+1)+(j+1));
+      var idx3 = 3*((i+1)*(this.div+1)+j);
+
+      // add N to the nbuffer for each vertex
+      this.nBuffer[idx1]+=prod[0]; //v1.x
+      this.nBuffer[idx1+1]+=prod[1]; //v1.y
+      this.nBuffer[idx1+2]+=prod[2]; //v1.z
+      this.nBuffer[idx2]+=prod[0]; //v2.x
+      this.nBuffer[idx2+1]+=prod[1]; //v2.y
+      this.nBuffer[idx2+2]+=prod[2]; //v2.z
+      this.nBuffer[idx3]+=prod[0]; //v3.x
+      this.nBuffer[idx3+1]+=prod[1]; //v3.y
+      this.nBuffer[idx3+2]+=prod[2]; //v3.z
+
+      // get the lower triangles vertices
+      this.getVertex(v1, i, j);
+      this.getVertex(v2, i, j+1);
+      this.getVertex(v3, i+1, j+1);
+
+      //Get the cross product of (v2-v1)x(v3-v1)
+      cross_prod(prod, [v2[0]-v1[0], v2[1]-v1[1], v2[2]-v1[2]], [v3[0]-v1[0], v3[1]-v1[1], v3[2]-v1[2]]);
+
+      // get index of each vertex in nBuffer
+      var idx1 = 3*(i*(this.div+1)+j);
+      var idx2 = 3*(i*(this.div+1)+(j+1));
+      var idx3 = 3*((i+1)*(this.div+1)+j+1);
+
+      // add N to the nbuffer for each vertex
+      this.nBuffer[idx1]+=prod[0]; //v1.x
+      this.nBuffer[idx1+1]+=prod[1]; //v1.y
+      this.nBuffer[idx1+2]+=prod[2]; //v1.z
+      this.nBuffer[idx2]+=prod[0]; //v2.x
+      this.nBuffer[idx2+1]+=prod[1]; //v2.y
+      this.nBuffer[idx2+2]+=prod[2]; //v2.z
+      this.nBuffer[idx3]+=prod[0]; //v3.x
+      this.nBuffer[idx3+1]+=prod[1]; //v3.y
+      this.nBuffer[idx3+2]+=prod[2]; //v3.z
+    }
+  }
+
+  // Then normalize each normal vector in the nBuffer
+  for(var x=0; x<=this.div; x++){
+    for(var y=0; y<=this.div; y++){
+      // get the curren idx
+      var idx = 3*(x*(this.div+1)+y);
+      //calculate magnitude
+      var magnitude = Math.sqrt(this.nBuffer[idx]*this.nBuffer[idx]+this.nBuffer[idx+1]*this.nBuffer[idx+1]+this.nBuffer[idx+2]*this.nBuffer[idx+2]);
+      //normalize x,y,x components
+      this.nBuffer[idx]=this.nBuffer[idx]/magnitude;
+      this.nBuffer[idx+1]=this.nBuffer[idx+1]/magnitude;
+      this.nBuffer[idx+2]=this.nBuffer[idx+2]/magnitude;
+    }
+  }
+}
+
+}
---
+++ mp2_p2/mp2.js
@@ -0,0 +1,386 @@
+/**
+ * @file A WebGl program that renders and animates a Block I or two circle gradients rotating
+ * @author Robert Krokos <rkroko2@illinois.edu>
+ */
+
+/** @global The WebGL context */
+var gl;
+
+/** @global The HTML5 canvas we draw on */
+var canvas;
+
+/** @global A simple GLSL shader program */
+var shaderProgram;
+
+var mat4 = glMatrix.mat4;
+
+var mat3 = glMatrix.mat3;
+
+var vec3 = glMatrix.vec3;
+
+var vec4 = glMatrix.vec4;
+
+/** @global The Modelview matrix */
+var mvMatrix = mat4.create();
+
+/** @global The Projection matrix */
+var pMatrix = mat4.create();
+
+/** @global The Normal matrix */
+var nMatrix = mat3.create();
+
+/** @global The matrix stack for hierarchical modeling */
+var mvMatrixStack = [];
+
+/** @global The angle of rotation around the y axis */
+var viewRot = 0;
+
+/** @global A glmatrix vector to use for transformations */
+var transformVec = vec3.create();
+
+// Initialize the vector....
+vec3.set(transformVec,0.0,0.0,-2.0);
+
+/** @global An object holding the geometry for a 3D terrain */
+var myTerrain;
+
+
+// View parameters
+/** @global Location of the camera in world coordinates */
+var eyePt = vec3.fromValues(0,0,-0);
+/** @global Direction of the view in world coordinates */
+var viewDir = vec3.fromValues(0.0,0,-1);
+/** @global Up vector for view matrix creation, in world coordinates */
+var up = vec3.fromValues(0.0,1.0,0.0);
+/** @global Location of a point along viewDir in world coordinates */
+var viewPt = vec3.fromValues(0.0,0.0,0.0);
+
+//Light parameters
+/** @global Light position in VIEW coordinates */
+var lightPosition = [0,3,3];
+/** @global Ambient light color/intensity for Phong reflection */
+var lAmbient = [0,0,0];
+/** @global Diffuse light color/intensity for Phong reflection */
+var lDiffuse = [1,1,1];
+/** @global Specular light color/intensity for Phong reflection */
+var lSpecular =[0,0,0];
+
+//Material parameters
+/** @global Ambient material color/intensity for Phong reflection */
+var kAmbient = [1.0,1.0,1.0];
+/** @global Array of diffuse material color/intensity for Phong reflection */
+var kTerrainDiffuse = [[52/255, 152/255, 235/255], //blue
+                        [101/255, 235/255, 52/255], //green
+                        [176/255, 134/255, 7/255], //brown
+                        [1, 1, 1]]; //white
+/** @global Specular material color/intensity for Phong reflection */
+var kSpecular = [0.0,0.0,0.0];
+/** @global Shininess exponent for Phong reflection */
+var shininess = 100;
+/** @global Edge color fpr wireframeish rendering */
+var kEdgeBlack = [0.0,0.0,0.0];
+/** @global Edge color for wireframe rendering */
+var kEdgeWhite = [1.0,1.0,1.0];
+
+
+/**
+ * Translates degrees to radians
+ * @param {Number} degrees Degree input to function
+ * @return {Number} The radians that correspond to the degree input
+ */
+function degToRad(degrees) {
+        return degrees * Math.PI / 180;
+}
+
+/**
+* Computes AxB and places in P
+* @param {Object} P an array of length 3 that will be AxB
+* @param {Object} A an array of length 3 holding x,y,z coords
+* @param {Object} B an array of length 3 holding x,y,z coords
+*/
+function cross_prod(P, A, B){
+  P[0] = A[1]*B[2]-A[2]*B[1];
+  P[1] = A[2]*B[0]-A[0]*B[2];
+  P[2] = A[0]*B[1]-A[1]*B[0];
+}
+
+
+/**
+ * Creates a context for WebGL
+ * @param {element} canvas WebGL canvas
+ * @return {Object} WebGL context
+ */
+function createGLContext(canvas) {
+  var context = null;
+  context = canvas.getContext("webgl");
+  if (context) {
+    // If context found, set viewport dimensions to match canvas
+    context.viewportWidth = canvas.width;
+    context.viewportHeight = canvas.height;
+  } else {
+    //else alert user
+    alert("Failed to create WebGL context!");
+    console.log("Error with WebGL context");
+  }
+  return context;
+}
+
+/**
+  * Pushes matrix onto MV matrix stack
+  */
+function mvPop(){
+  if(mvMatrixStack.length != 0){
+    mvMatrix = mvMatrixStack.pop();
+  }
+  else {
+    throw "Invalid MV PopMatrix!";
+  }
+}
+
+/**
+  * Pushes matrix onto MV matrix stack
+  */
+function mvPush(){
+  var copy = mat4.clone(mvMatrix);
+  mvMatrixStack.push(copy);
+}
+
+/**
+* Generates and sends the normal matrix to the shader
+*/
+function uploadNMatrix() {
+  mat3.fromMat4(nMatrix,mvMatrix);
+  mat3.transpose(nMatrix,nMatrix);
+  mat3.invert(nMatrix,nMatrix);
+  gl.uniformMatrix3fv(shaderProgram.nMatrixUniform, false, nMatrix);
+}
+
+/**
+  * Sends Projection Matrix to the shader program
+  */
+function uploadPMatrix(){
+  gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
+}
+
+/**
+  * Sends Model View Matrix to the shader program
+  */
+function uploadMVMatrix(){
+  gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
+}
+
+/**
+ * Sends material information to the shader
+ * @param {Float32} alpha shininess coefficient
+ * @param {Float32Array} a Ambient material color
+ * @param {Float32Array} d Array of 4 diffuse material colors
+ * @param {Float32Array} s Specular material color
+ */
+function setMaterialUniforms(alpha,a,d,s) {
+  gl.uniform1f(shaderProgram.uniformShininessLoc, alpha);
+  gl.uniform3fv(shaderProgram.uniformAmbientMaterialColorLoc, a);
+  gl.uniform3fv(shaderProgram.uniformDiffuseColorLoc1, d[3]);
+  gl.uniform3fv(shaderProgram.uniformDiffuseColorLoc2, d[2]);
+  gl.uniform3fv(shaderProgram.uniformDiffuseColorLoc3, d[1]);
+  gl.uniform3fv(shaderProgram.uniformDiffuseColorLoc4, d[0]);
+  gl.uniform3fv(shaderProgram.uniformSpecularMaterialColorLoc, s);
+}
+
+/**
+ * Sends light information to the shader
+ * @param {Float32Array} loc Location of light source
+ * @param {Float32Array} a Ambient light strength
+ * @param {Float32Array} d Diffuse light strength
+ * @param {Float32Array} s Specular light strength
+ */
+function setLightUniforms(loc,a,d,s) {
+  gl.uniform3fv(shaderProgram.uniformLightPositionLoc, loc);
+  gl.uniform3fv(shaderProgram.uniformAmbientLightColorLoc, a);
+  gl.uniform3fv(shaderProgram.uniformDiffuseLightColorLoc, d);
+  gl.uniform3fv(shaderProgram.uniformSpecularLightColorLoc, s);
+}
+
+/**
+ * Sends projection/modelview/normal matrices to shader
+ */
+function setMatrixUniforms() {
+  uploadMVMatrix();
+  uploadPMatrix();
+  uploadNMatrix();
+}
+
+/**
+* Loads and compiles shader from DOM
+* @param {string} id ID string for shader to load, either a vertex shader or fragment shader
+* @return {WebGLShader} the shader loaded
+*/
+function loadShaderFromDOM(id){
+  var script = document.getElementById(id);
+  if(!script){
+    // Fail if element with this ID not found
+    console.log("Failed to load shader by id: ", id);
+    return null;
+  }
+
+  // Loop through DOM element and load shader source code
+  var source = "";
+  var cur = script.firstChild;
+  while(cur){
+    if(cur.nodeType == 3 ) {source+=cur.textContent;}
+    cur =cur.nextSibling;
+  }
+
+  // Create approriate shader type based on DOM type
+  var shader;
+  if(script.type=="x-shader/x-fragment") {shader=gl.createShader(gl.FRAGMENT_SHADER);}
+  else if(script.type=="x-shader/x-vertex") {shader=gl.createShader(gl.VERTEX_SHADER);}
+  else {
+    console.log("Failed to get type: ", id);
+    return null;
+  }
+
+  // Set the source code and compile
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+
+  if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    // If not compiled successfully, alert and return null
+    console.log(gl.getShaderInfoLog(shader));
+    console.log(source);
+    console.log("Failed to load shader!");
+    return null;
+  }
+
+  return shader; // return the compiled shader
+}
+
+/**
+ * Setup the fragment and vertex shaders
+ */
+function setupShaders() {
+  //Load the vertex and fragment shaders
+  vertexShader =loadShaderFromDOM("blinn-phong-vs");
+  fragmentShader =loadShaderFromDOM("blinn-phong-fs");
+
+  //Create the shader program and attach the vertex/fragment shaders
+  shaderProgram = gl.createProgram();
+  gl.attachShader(shaderProgram, vertexShader);
+  gl.attachShader(shaderProgram, fragmentShader);
+  gl.linkProgram(shaderProgram);
+
+  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
+    //Alert if shaders not set up properly
+    alert("Failed to setup shaders");
+    console.log(gl.getProgramInfoLog(shaderProgram));
+  }
+
+  // Use the shaderProgram just created
+  gl.useProgram(shaderProgram);
+
+  // Get attributes from shaders and enable arrays that will be needed later
+  shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
+  gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
+
+  shaderProgram.vertexNormalAttribute = gl.getAttribLocation(shaderProgram, "aVertexNormal");
+  gl.enableVertexAttribArray(shaderProgram.vertexNormalAttribute);
+
+  // Get the uniforms
+  shaderProgram.mvMatrixUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+  shaderProgram.pMatrixUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+  shaderProgram.nMatrixUniform = gl.getUniformLocation(shaderProgram, "uNMatrix");
+  shaderProgram.uniformLightPositionLoc = gl.getUniformLocation(shaderProgram, "uLightPosition");
+  shaderProgram.uniformAmbientLightColorLoc = gl.getUniformLocation(shaderProgram, "uAmbientLightColor");
+  shaderProgram.uniformDiffuseLightColorLoc = gl.getUniformLocation(shaderProgram, "uDiffuseLightColor");
+  shaderProgram.uniformSpecularLightColorLoc = gl.getUniformLocation(shaderProgram, "uSpecularLightColor");
+  shaderProgram.uniformShininessLoc = gl.getUniformLocation(shaderProgram, "uShininess");
+  shaderProgram.uniformAmbientMaterialColorLoc = gl.getUniformLocation(shaderProgram, "uKAmbient");
+  shaderProgram.uniformDiffuseColorLoc1 = gl.getUniformLocation(shaderProgram, "kDiffuse1");
+  shaderProgram.uniformDiffuseColorLoc2 = gl.getUniformLocation(shaderProgram, "kDiffuse2");
+  shaderProgram.uniformDiffuseColorLoc3 = gl.getUniformLocation(shaderProgram, "kDiffuse3");
+  shaderProgram.uniformDiffuseColorLoc4 = gl.getUniformLocation(shaderProgram, "kDiffuse4");
+  shaderProgram.uniformSpecularMaterialColorLoc = gl.getUniformLocation(shaderProgram, "uKSpecular");
+
+}
+
+/**
+ * Draw call that applies matrix transformations to model and draws model in frame
+ */
+function draw() {
+  gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  // We'll use perspective
+  mat4.perspective(pMatrix,degToRad(45),
+                   gl.viewportWidth / gl.viewportHeight,
+                   0.1, 200.0);
+
+  // We want to look down -z, so create a lookat point in that direction
+  vec3.add(viewPt, eyePt, viewDir);
+  // Then generate the lookat matrix and initialize the MV matrix to that view
+  mat4.lookAt(mvMatrix,eyePt,viewPt,up);
+
+  //push current modelview matrix to stack
+  mvPush();
+
+  // generate transformation
+  vec3.set(transformVec, 0.0, -0.25, -2.0);
+  //add transformation to modelview
+  mat4.translate(mvMatrix, mvMatrix,transformVec);
+  // rotate modelview around y
+  mat4.rotateY(mvMatrix, mvMatrix, degToRad(viewRot));
+  // rotate around x
+  mat4.rotateX(mvMatrix, mvMatrix, degToRad(-75));
+  // Update the matrix uniforms to hold calculations just performed
+  setMatrixUniforms();
+  // Send the lighting information to shaders
+  setLightUniforms(lightPosition,lAmbient,lDiffuse,lSpecular);
+  // send material information to shaders
+  setMaterialUniforms(shininess,kAmbient,kTerrainDiffuse,kSpecular);
+  // draw the terrain
+  myTerrain.drawTriangles();
+  // Pop modelview from stack
+  mvPop();
+
+
+}
+
+/**
+ * Populate buffers with data
+ */
+function setupBuffers() {
+    myTerrain = new Terrain(150,-6 ,6,-6,6);
+    myTerrain.loadBuffers();
+}
+
+/**
+* Startup function called from html code to start program.
+*/
+function startup() {
+  console.log("Startup Function Called");
+
+  // get canvas to render model on
+  canvas = document.getElementById("myGLCanvas");
+
+  // create context
+  gl = createGLContext(canvas);
+
+  // Setup the shaders and buffers
+  setupShaders();
+  setupBuffers();
+
+  // Set the clear color to white
+  gl.clearColor(1, 1, 1, 1);
+
+  gl.enable(gl.DEPTH_TEST);
+
+  // Call tick function, which will then be called every subsequent animation frame
+  tick();
+}
+
+/**
+ * Tick called for every animation frame.
+ */
+function tick() {
+    requestAnimFrame(tick);
+    draw();
+}
